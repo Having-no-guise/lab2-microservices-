55dbc2c6534828e8908c8fa1a631b4365874b31f
Having-no-guise <eskelanir@mail.ru>
2025-10-09 22:38:45 +0700

    lab2

---
+++ serv2/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from service 2');
+});
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server2 running on http://localhost:3000');
+});
