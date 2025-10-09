c8140afbe0cbf4de2eb5c9b425126bd36233943b
Having-no-guise <eskelanir@mail.ru>
2025-10-09 22:39:32 +0700

    lab3

--- serv1/index.js
+++ serv1/index.js
@@ -1,10 +1,47 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const amqp = require('amqplib')
 
-app.get('/', (req, res) => {
-  res.send('Hello from service 1');
-});
+let connection
+let channel
+const queue = 'messages'
 
-app.listen(3000, '0.0.0.0', () => {
-  console.log('Server1 running on http://localhost:3000');
-});
+async function initRabbitMQ() {
+  connection = await amqp.connect('amqp://guest:guest@rabbitmq:5672')
+  channel = await connection.createChannel()
+  await channel.assertQueue(queue, { durable: false })
+  console.log('RabbitMQ initialized')
+}
+
+async function sendMessage(msg) {
+  if (!channel) {
+    console.error('Channel not initialized!')
+    return
+  }
+
+  channel.sendToQueue(queue, Buffer.from(msg))
+  console.log(`Sent: ${msg}`)
+}
+
+async function consumeMessages() {
+  console.log('Waiting for messages... (server1)')
+
+  channel.consume(queue, msg => {
+    if (msg !== null) {
+      console.log(`Received: ${msg.content.toString()}`)
+      channel.ack(msg)
+    }
+  })
+}
+
+app.get('/', async (req, res) => {
+  await sendMessage('Message from service 1')
+  res.send('Res from service 1')
+})
+
+initRabbitMQ().then(() => {
+  consumeMessages()
+  app.listen(3000, '0.0.0.0', () => {
+    console.log('Server1 running on http://localhost:3000')
+  })
+})
